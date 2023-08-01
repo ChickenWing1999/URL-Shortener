a72532976b33394b64adc5886875f93136d73d56
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-08-01 14:09:56 +0800

    Update urlcool.js

--- theme/urlcool/urlcool.js
+++ theme/urlcool/urlcool.js
@@ -6,7 +6,7 @@ let res
     }
 
     document.getElementById("searchbtn").disab
-	document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>';
+	document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
     fetch(window.location.pathname, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
