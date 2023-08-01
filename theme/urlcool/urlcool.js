0c381bd0cc1673127f60cf474fabd563b179f872
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-08-01 13:04:55 +0800

    Update urlcool.js

--- theme/urlcool/urlcool.js
+++ theme/urlcool/urlcool.js
@@ -6,7 +6,7 @@ let res
     }
 
     document.getElementById("searchbtn").disabled=true;
-	document.getElementById("searchbtn").innerHTML='<span role="status" aria-hidden="true"></span>Please wait...';
+	document.getElementById("searchbtn").innerHTML='Please wait...';
     fetch(window.location.pathname, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
